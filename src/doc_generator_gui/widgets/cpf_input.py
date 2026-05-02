367949150f10e175a244b98ef93ece329a813dea
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-05-02 16:20:00 -0300

    Reorganize cpf_input code.

--- src/doc_generator_gui/widgets/cpf_input.py
+++ src/doc_generator_gui/widgets/cpf_input.py
@@ -7,33 +7,36 @@ class Cpf_Input(QtWidgets.QLineEdit):
         self.setPlaceholderText(
             "Observação: Digite apenas os números do CPF, formatação automática."
         )
-        self.textChanged.connect(self.format_text)
+        self.textEdited.connect(self.format_text)
         self.setMaxLength(18)
 
     def format_text(self):
-        text = self.text()
-
         # Remove any non-digit characters
-        text = "".join(filter(str.isdigit, text))
+        text = "".join(filter(str.isdigit, self.text()))
 
         # Format as a common format for both CPF and CNPJ (e.g., 123.456.789-01 or 12.345.678/9012-34)
-        if len(text) <= 11:
-            # Format as CPF
-            if len(text) >= 4:
-                text = f"{text[:3]}.{text[3:]}"
-            if len(text) >= 8:
-                text = f"{text[:7]}.{text[7:]}"
-            if len(text) >= 12:
-                text = f"{text[:11]}-{text[11:]}"
-        else:
-            # Format as CNPJ
-            if len(text) >= 3:
-                text = f"{text[:2]}.{text[2:]}"
-            if len(text) >= 7:
-                text = f"{text[:6]}.{text[6:]}"
-            if len(text) >= 11:
-                text = f"{text[:10]}/{text[10:]}"
-            if len(text) >= 16:
-                text = f"{text[:15]}-{text[15:]}"
+        formatted = (
+            self.format_as_cpf(text) if len(text) <= 11 else self.format_as_cnpj(text)
+        )
 
-        self.setText(text)
+        self.setText(formatted)
+
+    def format_as_cpf(self, text: str) -> str:
+        if len(text) >= 4:
+            text = f"{text[:3]}.{text[3:]}"
+        if len(text) >= 8:
+            text = f"{text[:7]}.{text[7:]}"
+        if len(text) >= 12:
+            text = f"{text[:11]}-{text[11:]}"
+        return text
+
+    def format_as_cnpj(self, text: str) -> str:
+        if len(text) >= 3:
+            text = f"{text[:2]}.{text[2:]}"
+        if len(text) >= 7:
+            text = f"{text[:6]}.{text[6:]}"
+        if len(text) >= 11:
+            text = f"{text[:10]}/{text[10:]}"
+        if len(text) >= 16:
+            text = f"{text[:15]}-{text[15:]}"
+        return text
