5967cfe936422adb177ed9030a6e621dfcf708e2
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-05-08 11:40:32 -0300

    fix GetValueFromLayout: remove current layout parameter and use current layout from layout_store.

--- src/doc_generator_gui/Stores/layout_store.py
+++ src/doc_generator_gui/Stores/layout_store.py
@@ -17,14 +17,14 @@ class LayoutStore:
     def GetAllLayouts(self):
         return self.__layouts
 
-    def GetValueFromLayout(self, cur_layout: dict, index: int, key: str):
+    def GetValueFromLayout(self, index: int, key: str):
         """
         we convert our dictionary items in a list, then we returns the
         data from the dictionary based on the index of a parent key,
         by selecting the corresponding key.
         """
 
-        layout_data = list(cur_layout.items())[index][1]
+        layout_data = list(self.__cur_layout.items())[index][1]
         return layout_data[key] if key in layout_data else ""
 
     def GetCurrentLayout(self) -> dict:
