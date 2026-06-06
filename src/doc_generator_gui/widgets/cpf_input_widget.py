42a2073a62051a77c795f2b7a007e0e90c679721
Endryl Richard Monteiro <endrylrm@hotmail.com>
2026-06-06 14:15:53 -0300

    change methods of CpfInputWidget to internal/private.

--- src/doc_generator_gui/widgets/cpf_input_widget.py
+++ src/doc_generator_gui/widgets/cpf_input_widget.py
@@ -9,10 +9,10 @@ class CpfInputWidget(QtWidgets.QLineEdit):
 
     def __init__(self, parent=None):
         super().__init__(parent=parent)
-        self.textEdited.connect(self.formatText)
+        self.textEdited.connect(self._formatText)
         self.setMaxLength(18)
 
-    def formatText(self):
+    def _formatText(self):
         cursor = self.cursorPosition()
 
         # Remove any non-digit characters and limit to 14 characters
@@ -23,15 +23,15 @@ class CpfInputWidget(QtWidgets.QLineEdit):
 
         # Format as a common format for both CPF and CNPJ (e.g., 123.456.789-01 or 12.345.678/9012-34)
         formatted = (
-            self.formatAsCPF(text) if len(text) <= 11 else self.formatAsCNPJ(text)
+            self._formatAsCPF(text) if len(text) <= 11 else self._formatAsCNPJ(text)
         )
 
         self.setText(formatted)
 
-        newCursor = self.cursorFromDigits(formatted, digitsBeforeCursor)
+        newCursor = self._cursorFromDigits(formatted, digitsBeforeCursor)
         self.setCursorPosition(newCursor)
 
-    def formatAsCPF(self, text: str) -> str:
+    def _formatAsCPF(self, text: str) -> str:
         if len(text) >= 4:
             text = f"{text[:3]}.{text[3:]}"
         if len(text) >= 8:
@@ -40,7 +40,7 @@ class CpfInputWidget(QtWidgets.QLineEdit):
             text = f"{text[:11]}-{text[11:]}"
         return text
 
-    def formatAsCNPJ(self, text: str) -> str:
+    def _formatAsCNPJ(self, text: str) -> str:
         if len(text) >= 3:
             text = f"{text[:2]}.{text[2:]}"
         if len(text) >= 7:
@@ -51,7 +51,7 @@ class CpfInputWidget(QtWidgets.QLineEdit):
             text = f"{text[:15]}-{text[15:]}"
         return text
 
-    def cursorFromDigits(self, formatted: str, digitsCount: int) -> int:
+    def _cursorFromDigits(self, formatted: str, digitsCount: int) -> int:
         if digitsCount <= 0:
             return 0
 
